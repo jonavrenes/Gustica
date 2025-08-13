b30ca763a16b8666e672cfb3e9ff6660e77680f1
jonavrenes <jonaaaaa28@gmail.com>
2025-08-13 13:54:31 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -30,36 +29,3 @@ def contacto():
 
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-@app.route('/')
-def inicio():
-    return render_template('index.html')
-
-@app.route('/quienes')
-def quienes():
-    return render_template('quienes.html')
-
-@app.route('/productos')
-def productos():
-    return render_template('productos.html')
-
-@app.route('/carrito')
-def carrito():
-    return render_template('carrito.html')
-
-@app.route('/referencias')
-def referencias():
-    return render_template('referencias.html')
-
-@app.route('/contacto')
-def contacto():
-    return render_template('contacto.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> 1e8644f891b8f5d5f94c155bb722e7cd69a672fb
